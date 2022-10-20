b42820bc29ec078b1e0796a17e1a3a229ce3b6bc
rajibahmed1997 <saidulislamrajib1997@gmail.com>
2022-10-20 11:57:33 +0600

    Created Repository Pattern on Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+
+
+        // PERFORM CRUD OPERATION
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
